afeb1dbfb60c02e01ecbe6aad898a1f192a24c30
Patryk Mezydlo <mezydlo.p@gmail.com>
2017-01-16 03:43:36 +0100

    checking connection whit oscilloscope
    Signed-off-by: Patryk Mezydlo <mezydlo.p@gmail.com>

---
+++ oscilloscope_data_cap.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+from telnetlib_receive_all import Telnet
+import sys
+import time
+import os
+
+port = 5555
+osc_ip = "192.168.20.15"
+
+response = os.system("ping -c 1 " + osc_ip + " > /dev/null")
+
+if response != 0:
+    print "No response ping " + osc_ip
+    sys.exit("ERROR")
+
+print "OK"
+
