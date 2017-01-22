80375b36d2450deaf43ee9a1c3564419d68be975
Patryk Mezydlo <mezydlo.p@gmail.com>
2017-01-22 15:59:49 +0100

    connecting with database using postgres
    Signed-off-by: Patryk Mezydlo <mezydlo.p@gmail.com>

--- oscilloscope_data_cap.py
+++ oscilloscope_data_cap.py
@@ -7,77 +7,47 @@ import time
 import os
 import numpy
 import matplotlib.pyplot as plot
+import psycopg2
 
 port = 5555
 osc_ip = "192.168.20.15"
 
-ret = os.system("ping -c 1 " + osc_ip + " > /dev/null")
+try:
 
-if ret != 0:
-    print "No response ping " + osc_ip
-    sys.exit("ERROR")
+    ret = os.system("ping -c 1 " + osc_ip + " > /dev/null")
 
-tn = Telnet(osc_ip, port);
-id = command(tn, "*IDN?")
+    if ret != 0:
+        print "No response ping " + osc_ip
+        sys.exit("ERROR")
 
-if id == "command error":
-    print "No response for Oscilloscope"
-    sys.exit("ERROR")
+    tn = Telnet(osc_ip, port);
+    id = command(tn, "*IDN?")
 
-ids = id.split(',')
-print "Manufacturer: " + ids[0]
-print "Device: " + ids[1]
-print "Serial number: " + ids[2]
-print "Firmware version: " + ids[3]
+    con = psycopg2.connect(database='osci_meas', user='pmezydlo')
 
-command(tn, ":STOP")
+    if id == "command error":
+        print "No response for Oscilloscope"
+        sys.exit("ERROR")
 
-chan = []
-for channel in ["CHAN1", "CHAN2", "CHAN3", "CHAN4", "MATH"]:
-    ret = command(tn, ":" + channel + ":DISP?")
+    ids = id.split(',')
+    print "Manufacturer: " + ids[0]
+    print "Device: " + ids[1]
+    print "Serial number: " + ids[2]
+    print "Firmware version: " + ids[3]
 
-    if ret == '1\n':
-        chan += [channel]
-        print channel + " is active"
+    cur = con.cursor()
+    query = "INSERT INTO device VALUES(DEFAULT, %s, %s, %s, %s, %s, %s)"
+    data = (ids[0],  ids[1],  ids[2], ids[3], osc_ip, port)
+    cur.execute(query, data)
+    con.commit()
 
-time_scale = float(command(tn, ":TIM:SCAL?"))
-time_offset = float(command(tn, ":TIM:OFFS?"))
+    
 
-print "Time scale: %.4f time offset: %.8fms" % (time_scale, time_offset)
+except psycopg2.DatabaseError, e:
+    print 'Error %s' % e
+    sys.exit(1)
 
-for channel in chan: 
-    volt_scale = float(command(tn, ":" + channel + ":SCAL?"))
-    volt_offset = float(command(tn, ":" + channel + ":OFFS?"))
+finally:
 
-    print channel + " offset value: %.4f scale value: %.4f" % (volt_offset, volt_scale)
-
-    command(tn, ":WAV:POIN:MODE RAW")
-    rawdata = command(tn, ":WAV:DATA? " + channel)[10:]
-    command(tn, ":KEY:FORCE")
-
-    size = len(rawdata)
-
-    print "Data size: %d" % size
-
-    data = numpy.frombuffer(rawdata, 'B')
-    data = data + 255
-    data = (data - 130.0 - volt_offset / volt_scale * 25) / 25 * volt_scale
-    print data
-    time = numpy.linspace(time_offset - 6 * time_scale, time_offset + 6 * time_scale, num = len(data))
-
-
-#    if time[-1] < 1e-3:
-#        time = time * 1e6
-#        tUnit = "uS"
-#    elif time[-1] < 1:
-#        time = time * 1e3
-#        tUnit = "mS"
-#    else:
-#        tUnit = "S"
-
-    plot.plot(time, data)
-    plot.title("Oscilloscope " + channel)
-    plot.ylabel("Voltage (V)")
- #   plot.xlabel("Time ["+tUnit+"]")
-    plot.xlim(time[0], time[-1])
-    plot.savefig('plot_'+channel+'.png')
+    if con:
+        con.close()
--- psql_create_table.sql
+++ psql_create_table.sql
@@ -7,19 +7,21 @@ DROP TABLE one CASCADE;
 
 CREATE TABLE device
 (
-	dev_ID          INT         NOT NULL,
+	dev_ID          SERIAL,
 	manufacturer    char(50),
 	name            char(50),
 	serial          char(50),
 	firmware_ver    char(50),
+        address         inet,
+        port            int,
         CONSTRAINT      PK_dev      PRIMARY KEY(dev_ID)
 );
 
 CREATE TABLE measurements
 (
-	meas_ID      	INT         NOT NULL,
+	meas_ID      	SERIAL,
         meas_comments   TEXT,
-        meas_dev_ID     INT         NOT NULL,
+        meas_dev_ID     SERIAL,
         meas_timestamp  TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT      PK_meas_ID  PRIMARY KEY(meas_ID),
         CONSTRAINT      FK_meas_ID  FOREIGN KEY(meas_dev_ID) REFERENCES device(dev_ID) ON UPDATE CASCADE
@@ -27,12 +29,12 @@ CREATE TABLE measurements
 
 CREATE TABLE one
 (	
-	one_ID  	INT         NOT NULL,
-        one_meas_ID     INT         NOT NULL,
-        one_interval    INT         NOT NULL,
-        one_Vpp    REAL        NOT NULL,
-        one_Vmax   REAL        NOT NULL,
-        one_Vmin   REAL        NOT NULL,
+	one_ID  	SERIAL,
+        one_meas_ID     SERIAL,
+        one_interval    INT,
+        one_Vpp         REAL,
+        one_Vmax        REAL,
+        one_Vmin        REAL,
         CONSTRAINT      PK_one_meas PRIMARY KEY(one_ID),
         CONSTRAINT      FK_one_meas FOREIGN KEY(one_meas_ID) REFERENCES measurements(meas_ID) ON UPDATE CASCADE
 );
